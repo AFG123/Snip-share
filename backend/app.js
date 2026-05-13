508d3a1de903b5de0aa13113c9c5990a591274a4
AFG123 <aryan123damai@gmail.com>
2026-05-13 21:21:34 +0530

    SnipShare Deployment fixes P4

--- backend/app.js
+++ backend/app.js
@@ -7,14 +7,14 @@ const { pool } = require("./src/services/db");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST", "DELETE"],
-    credentials: true,
-  })
-);
-//app.use(cors());
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL,
+//     methods: ["GET", "POST", "DELETE"],
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 app.use(express.json());
 app.use("/api/snippets", snippetRoutes);
 
