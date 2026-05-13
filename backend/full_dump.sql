14ea9b5afc2dcd2dd87d52fa987c38043e2ebd2c
AFG123 <aryan123damai@gmail.com>
2026-05-13 12:57:43 +0530

    SnipShare Deployment fixes

--- backend/full_dump.sql
+++ backend/full_dump.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict uZhFb2h0c9E8musHuQ4fCBqE6zTd0zvFn0vf4X0nk7UI6gr1xz5aIeFUuzA2re7
+\restrict t5rhYvIiCOnXn1O2S8rfVKMZLU4INUkxGNCyfl3reLMseo1BurCuv57AP6ozY09
 
 -- Dumped from database version 18.2
 -- Dumped by pg_dump version 18.2
@@ -50,7 +50,9 @@ CREATE TABLE public.snippets (
     burn_after_read boolean DEFAULT false,
     download_enabled boolean DEFAULT true NOT NULL,
     manage_token character varying(32),
-    view_count integer DEFAULT 0
+    view_count integer DEFAULT 0,
+    title character varying(60),
+    note character varying(140)
 );
 
 
@@ -95,6 +97,10 @@ XW4xLl	2026-05-10 22:28:34.480471
 uGAbBk	2026-05-10 22:32:04.626128
 Nqlq7d	2026-05-10 22:36:21.612107
 L7ENVH	2026-05-10 23:00:10.360491
+sCkVGj	2026-05-10 23:43:21.905784
+oo14Q6	2026-05-13 09:12:02.48221
+gNzTh6	2026-05-13 09:41:00.327091
+JUR7td	2026-05-13 09:43:36.152845
 \.
 
 
@@ -102,36 +108,8 @@ L7ENVH	2026-05-10 23:00:10.360491
 -- Data for Name: snippets; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.snippets (id, short_id, content, language, expiry_at, created_at, password_hash, burn_after_read, download_enabled, manage_token, view_count) FROM stdin;
-1	yDzbkF	console.log('Hello SnipShare')	javascript	2026-05-10 23:37:02.795	2026-05-09 23:37:02.844252	\N	f	t	\N	0
-2	GUhCXT	CREATE TABLE snippets (\n    id SERIAL PRIMARY KEY,\n    short_id VARCHAR(6) UNIQUE NOT NULL,\n    content TEXT NOT NULL,\n    language VARCHAR(50),\n    expiry_at TIMESTAMP,\n    created_at TIMESTAMP DEFAULT NOW()\n);	sql	2026-05-10 00:45:00.784	2026-05-09 23:45:00.8137	\N	f	t	\N	0
-3	djJgrC	feqrfefqerfefqrfecfc	plaintext	2026-05-10 23:46:24.954	2026-05-09 23:46:25.091308	\N	f	t	\N	0
-4	qlG0pa	res.setHeader("Content-Type", "text/plain");\nres.send(snippet.content);	javascript	2026-05-10 23:52:00.701	2026-05-09 23:52:00.838748	\N	f	t	\N	0
-5	vW1mcA	res.setHeader("Content-Type", "text/plain");\nres.send(snippet.content);	javascript	2026-05-10 23:53:43.413	2026-05-09 23:53:43.461693	\N	f	t	\N	0
-6	ZFj8R6	awewewqefqewrfeq	plaintext	2026-05-10 00:58:27.842	2026-05-09 23:58:27.887051	\N	f	t	\N	0
-7	5op8F6	ferfqefqrfqefqerfewgtebwtrggtgw	javascript	2026-05-11 00:07:17.665	2026-05-10 00:07:17.69518	\N	f	t	\N	0
-8	TcSLsy	console.log('Hello SnipShare')	javascript	2026-05-11 00:28:42.534	2026-05-10 00:28:42.763837	$2b$10$oruUMDt5O8JdKxKhSak.gep8wLwk9b973OKlm410E7NXsNmWzMKJG	f	t	\N	0
-9	yilGo7	rfqwewefwef	javascript	2026-05-11 00:34:23.099	2026-05-10 00:34:23.143683	\N	f	t	\N	0
-10	SJ0HDF	deewdewdwdew	javascript	2026-05-10 01:35:08.212	2026-05-10 00:35:08.257913	\N	f	t	\N	0
-11	Eon3EC	wfqerferqrfqrf	plaintext	2026-05-10 01:41:23.92	2026-05-10 00:41:24.117647	$2b$10$z1oe67.HKrrF0lbzPQWBp.xpU0n7YYdFr/KIFuptk5hpA.P.av/wu	f	t	\N	0
-12	IHysy0	qrwefrefqerf	javascript	2026-05-11 00:42:36.669	2026-05-10 00:42:36.752468	$2b$10$fWBAxM1M9u/BBeAkKyeoquK7ysBjyIsvZCcEW9oYbUFKxFBpRF4oW	f	t	\N	0
-13	QqgpVm	wfqwefqwerfqrfrqef	javascript	2026-05-11 00:48:37.18	2026-05-10 00:48:37.383467	$2b$10$vU8lygts9GJHP/pvzNfAiu3k9424AQaEHYw80F0mE8ZzIDiGhrAIi	f	t	\N	0
-14	q61vhS	Create a new endpoint:\n\nPOST /api/snippets/:shortId/verify\n\nRequirements:\n- Accept "password" in body\n- Compare with password_hash using bcrypt.compare\n- If correct:\n  - Return full snippet content\n- If wrong:\n  - Return 401 "Invalid password"\n\nAdd this in:\n- routes\n- controller\n- service	javascript	2026-05-10 01:53:50.511	2026-05-10 00:53:50.614152	$2b$10$DFF6dTqZ9CF4.rIwN5nOOuQItggPanwIZhs1oCTzqZnG.4n9kLHJe	f	t	\N	0
-15	gl1gGW	if 5 > 2:\n print("Five is greater than two!") \nif 5 > 2:\n        print("Five is greater than two!") \n	python	2026-05-10 12:37:54.878	2026-05-10 11:37:54.99869	$2b$10$cSYLkt4h2TEJn6OecyRrFOLGZXjbk0n6VcsAGvHIUxzEdSecz4MyK	f	t	\N	0
-16	x7HV1K	wefweaafawef	javascript	2026-05-11 11:38:48.42	2026-05-10 11:38:48.452206	\N	f	t	\N	0
-17	CEWYuQ	ewdwedwdwed	javascript	2026-05-11 11:39:21.417	2026-05-10 11:39:21.503447	$2b$10$fAZIPV9nLdHIuInSDzYqk.xd/9UKKReGITgbmf0onhLUipmIFF.he	f	t	\N	0
-19	vQjkCV	wdqwefqewfdqw	plaintext	2026-05-11 13:27:33.499	2026-05-10 13:27:33.532199	\N	f	t	\N	0
-20	3piR3l	if 5 > 2:\n print("Five is greater than two!")\n        print("Five is greater than two!")\n	python	2026-05-10 15:39:55.86	2026-05-10 14:39:55.997099	\N	f	t	\N	0
-21	cS9Efm	wfqwefewfqr	javascript	2026-05-10 23:20:28.353	2026-05-10 22:20:28.4017	\N	f	t	\N	0
-22	t1dYzs	egreferfraeaee	javascript	2026-05-10 22:51:26.888	2026-05-10 22:27:27.03257	\N	f	t	\N	0
-26	6MWu3M	frfrffqferfeewrfwrf	javascript	2026-05-11 22:56:36.608	2026-05-10 22:56:36.653419	\N	f	t	rYxjBAkGgyuW9pBtBdDJ0uvpC1GjUcRB	0
-27	GeZejA	wdqwecqewc	javascript	2026-05-11 22:57:17.342	2026-05-10 22:57:17.432533	$2b$10$TzZ.mo7TWQ3JuEb/v7FauOnAplNhnW1u/0icOtx8vEA92QnRc1fKC	f	f	WvL2V4De7EyJ5oId0ofe12Hq7tXqQP7p	1
-29	d7buSz	fevevfeverv	javascript	2026-05-11 23:01:43.716	2026-05-10 23:01:43.854344	\N	f	f	3k8U6rJSZXCEJ0ElUh0iiYaDCVZPVA3S	0
-30	psFYcU	dcedcqecqec	javascript	2026-05-11 23:02:03.514	2026-05-10 23:02:03.552032	\N	f	t	OdOjCvpRRwQMgfcp0Xmw460IOKvPwsOM	0
-32	lCFfxd	hbjhbkjnkjn	javascript	2026-05-11 23:13:26.227	2026-05-10 23:13:26.367661	\N	f	t	aYVQ1InBbxBW2eSLlNrpCHORdP4QQL4S	0
-33	6Dodmx	dcwdwecwed	javascript	2026-05-11 23:17:24.879	2026-05-10 23:17:24.90973	\N	f	t	VMToisr5L3FtzKxCPc2ZCgyyjWHD1nmV	0
-34	0hyC7E	lkmlmkmlkm	javascript	2026-05-11 00:21:39.799	2026-05-10 23:21:39.832228	\N	f	t	lBwQny9wLewKBf3zKsCuhE4Yin5sw1yr	2
-35	H3yvOG	kjbkjnjkj	javascript	2026-05-11 23:23:02.091	2026-05-10 23:23:02.092066	\N	f	t	lxRpnAxnf2DxmLuexIPUYDjoGlYYFLCv	0
+COPY public.snippets (id, short_id, content, language, expiry_at, created_at, password_hash, burn_after_read, download_enabled, manage_token, view_count, title, note) FROM stdin;
+39	PjcVnk	3rf3f3wfergerg	javascript	2026-05-14 09:13:22.35	2026-05-13 09:13:22.435878	$2b$10$hakh08Hghy/MO2R35F7VY.0EwUozh29xc3Wdb2NcyitXj0O2RMfGy	f	f	YT2lHJoljjSbaiXO11SrxmD0vexDhqwq	1	\N	\N
 \.
 
 
@@ -139,7 +117,7 @@ COPY public.snippets (id, short_id, content, language, expiry_at, created_at, pa
 -- Name: snippets_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.snippets_id_seq', 35, true);
+SELECT pg_catalog.setval('public.snippets_id_seq', 41, true);
 
 
 --
@@ -178,5 +156,5 @@ ALTER TABLE ONLY public.snippets
 -- PostgreSQL database dump complete
 --
 
-\unrestrict uZhFb2h0c9E8musHuQ4fCBqE6zTd0zvFn0vf4X0nk7UI6gr1xz5aIeFUuzA2re7
+\unrestrict t5rhYvIiCOnXn1O2S8rfVKMZLU4INUkxGNCyfl3reLMseo1BurCuv57AP6ozY09
 
